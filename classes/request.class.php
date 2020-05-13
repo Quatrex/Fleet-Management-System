2b70eb0b8bbfad8f880ef83072f486b3444c5973
Nagitha Abeywickrema <nagitha98@gmail.com>
2020-05-13 13:59:34 +0530

    branch test1

--- classes/request.class.php
+++ classes/request.class.php
@@ -1,4 +1,16 @@
 <?php
 class Request
 {
+    private int $requestID; 
+    private $createdDate; //how handle 'date' in php? 
+    private $date; //how handle 'date' in php? 
+    private $time; //how handle 'time' in php? 
+    private string $dropLocation;
+    private string $pickLocation;
+    private string $purpose; 
+    private string $requesterID; //EmpID
+    private string $comments; //recheck implementation of  "HashMap(String,Employee)"
+    private int $state;
+    private string $justifiedBy; //EmpID
+    private string $approvedBy; //EmpID
 }
--- classes/requester.class.php
+++ classes/requester.class.php
@@ -1,4 +1,14 @@
 <?php
 class Requester extends Employee implements IRequestable
 {
+    function __construct($empID, $firstName, $lastName, $position, $email, $username, $password)
+    {
+        parent::__construct($empID, $firstName, $lastName, $position, $email, $username, $password);
+
+        //incluce required viewer and controller classes
+    }
+
+    public function placeRequest($date,$time,$dropLocation,$pickLocation,$purpose){
+        //continue
+    }
 }
