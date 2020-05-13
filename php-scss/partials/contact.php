863823f78b147c60e4ed72df4870b09acb327c52
poorna2152 <poorna2152@gmail.com>
2020-05-13 21:56:46 +0530

    Scss and php code blocked

---
+++ php-scss/partials/contact.php
@@ -0,0 +1,61 @@
+<!--Start of Cards-->
+<div class="container-fluid">
+    <!-- End Row -->
+    <div class="row">
+        <div class="col-12 m-b-30">
+            <h4>Officers In Charge</h4>
+            <div class="row">
+                <div class="col-md-6 col-lg-3">
+                    <div class="card">
+                        <img class="img-fluid" src="images/default-user-image.png" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">Justifying Officer</h5>
+                            <p class="card-text">Name:</p>
+                            <p class="card-text">Contact No:</p>
+                            <p class="card-text">Email:</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- End Col -->
+                <div class="col-md-6 col-lg-3">
+                    <div class="card">
+                        <img class="img-fluid" src="images/default-user-image.png" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">CAO</h5>
+                            <p class="card-text">Name:</p>
+                            <p class="card-text">Contact No:</p>
+                            <p class="card-text">Email:</p>
+
+                        </div>
+                    </div>
+                </div>
+                <!-- End Col -->
+                <div class="col-md-6 col-lg-3">
+                    <div class="card">
+                        <img class="img-fluid" src="images/default-user-image.png" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">DCAO</h5>
+                            <p class="card-text">Name:</p>
+                            <p class="card-text">Contact No:</p>
+                            <p class="card-text">Email:</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- End Col -->
+                <div class="col-md-6 col-lg-3">
+                    <div class="card">
+                        <img class="img-fluid" src="images/default-user-image.png" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">Vehicle Pool Management Officer</h5>
+                            <p class="card-text">Name:</p>
+                            <p class="card-text">Contact No:</p>
+                            <p class="card-text">Email:</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- End Col -->
+            </div>
+        </div>
+    </div>
+    <!-- End Row -->
+</div>
---
+++ php-scss/partials/footer.php
@@ -0,0 +1,20 @@
+<!--**********************************
+            Footer start
+    ***********************************-->
+<div class="footer">
+    <div class="copyright">
+        <p>Copyright &copy; Designed & Developed by <a href="https://themeforest.net/user/quixlab">Quatrex</a> 2020</p>
+    </div>
+</div>
+
+<!--**********************************
+            Footer end
+    ***********************************-->
+
+</div>
+<?php include 'js.php';
+?>
+
+</body>
+
+</html>
