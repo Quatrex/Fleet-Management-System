2c7647e88ffc9a597e87c7047215369ca6472288
Nagitha Abeywickrema <nagitha98@gmail.com>
2020-05-14 10:15:11 +0530

    defined MVC model classes

---
+++ classes/dbh.class.php
@@ -0,0 +1,21 @@
+<?php
+class DBH {
+    //initialize in constructor
+    private $servername = 'remotemysql.com';
+    private $dBUsername = 'Kvs8AuC78e';
+    private $dBPassword = 'bmMrp4oj2h';
+    private $dBName = 'Kvs8AuC78e';
+
+    protected function connect(){
+        $conn = mysqli_connect($this->$servername, $this->$dBUsername, $this->$dBPassword, $this->$dBName);
+
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+        else{
+            return $conn;
+        }
+
+    }
+
+}
---
+++ classes/viewer.class.php
@@ -0,0 +1,5 @@
+<?php
+
+class Viewer extends Model{
+    
+}
