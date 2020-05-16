40f8a630a01430be24c5a2a3e93b5bdde2b4e5ed
Nagitha Abeywickrema <nagitha98@gmail.com>
2020-05-16 18:18:13 +0530

    implemented getRecordsEmp

--- classes/viewer.class.php
+++ classes/viewer.class.php
@@ -4,4 +4,8 @@ class Viewer extends Model{
     public function getRecords($requesterID,$status){
         return parent::getRecords($requesterID,$status);
     }
+
+    public function getRecordsEmp($position){
+        return parent::getRecordsEmp($position);
+    }
 }
