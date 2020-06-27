78a64d8e3a3c65804289a269b999762b7b9aa0f5
HirumalPriyashan <hirupriyashanrc@gmail.com>
2020-06-27 23:17:46 +0530

    ADD - CAO & VPMO to index.php

--- index.php
+++ index.php
@@ -8,6 +8,12 @@ else{
     if ($_SESSION['position']=='jo') {
         header('Location: ./Layout/jo.php');
     }
+    elseif ($_SESSION['position']=='cao') {
+        header('Location: ./Layout/cao.php');
+    }
+    elseif ($_SESSION['position']=='vpmo') {
+        header('Location: ./Layout/vpmo.php');
+    }
     else {
         header('Location: ./Layout/requester.php');
     }
