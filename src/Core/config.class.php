b7fcdcc8752434e8cb78d8873c666388764e4758
nipunayf <nipuna.18@cse.mrt.ac.lk>
2020-11-29 18:17:14 +0530

    MOD - Changed database

--- src/Core/config.class.php
+++ src/Core/config.class.php
@@ -2,9 +2,9 @@
 namespace Core;
 
 class Config{
-    static string $host = 'remotemysql.com';
-    static string $user = 'Kvs8AuC78e';
-    static string $pass = 'bmMrp4oj2h';
-    static string $dbname = 'Kvs8AuC78e';
+    static string $host = 'sql12.freemysqlhosting.net';
+    static string $user = 'sql12379084';
+    static string $pass = '2mW6DS3xP8';
+    static string $dbname = 'sql12379084';
     static string $port = '3306';
 }
